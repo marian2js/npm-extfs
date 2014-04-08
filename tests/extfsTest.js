4503ea14ace235deaadbf2d8a60988e6979b042c
Mariano Pardo <marianomdq1@gmail.com>
2014-04-08 14:30:48 -0300

    Improved unit tests

--- tests/extfsTest.js
+++ tests/extfsTest.js
@@ -1,27 +1,30 @@
 var expect = require('expect.js');
+var path = require('path');
 var fs = require('../extfs');
 
 describe('extfs', function () {
+  var rootPath = path.join(__dirname, '../');
+
 	it('should return all directories', function (done) {
-		fs.getDirs('../', function (err, dirs) {
-			expect(dirs).to.be.an('array');
-			expect(dirs.length > 0).to.be.ok();
+		fs.getDirs(rootPath, function (err, dirs) {
+			expect(dirs).to.be.an(Array);
+			expect(dirs.length).to.be.greaterThan(0);
 			done();
 		});
 	});
 
 	it('should return all directories sync', function () {
-		var dirs = fs.getDirsSync('../');
-		expect(dirs).to.be.an('array');
+		var dirs = fs.getDirsSync(rootPath);
+		expect(dirs).to.be.an(Array);
 		expect(dirs.length > 0).to.be.ok();
 	});
 
 	it('should check if a file is empty', function (done) {
-		var notEmptyFile = '../README.md';
+		var notEmptyFile = path.join(__dirname, '../README.md');
 		var emptyFile = './AN EMPTY FILE';
-		fs.isEmpty (notEmptyFile, function (empty) {
+		fs.isEmpty(notEmptyFile, function (empty) {
 			expect(empty).to.be(false);
-			fs.isEmpty (emptyFile, function (empty) {
+			fs.isEmpty(emptyFile, function (empty) {
 				expect(empty).to.be(true);
 				done();
 			});
@@ -29,7 +32,7 @@ describe('extfs', function () {
 	});
 
 	it('should check if a file is empty sync', function () {
-		var notEmptyFile = '../README.md';
+    var notEmptyFile = path.join(__dirname, '../README.md');
 		var emptyFile = './AN EMPTY FILE';
 		var empty = fs.isEmptySync(notEmptyFile);
 		expect(empty).to.be(false);
@@ -38,7 +41,7 @@ describe('extfs', function () {
 	});
 
 	it('should check if a directory is empty', function (done) {
-		var notEmptyDir = './';
+		var notEmptyDir = __dirname;
 		var emptyDir = './AN EMPTY DIR';
 		fs.isEmpty(notEmptyDir, function (empty) {
 			expect(empty).to.be(false);
@@ -50,16 +53,13 @@ describe('extfs', function () {
 	});
 
 	it('should check if a directory is empty sync', function () {
-		var notEmptyDir = './';
+		var notEmptyDir = __dirname;
 		var emptyDir = './AN EMPTY DIR';
-		var empty = fs.isEmptySync(notEmptyDir);
-		expect(empty).to.be(false);
-		empty = fs.isEmptySync(emptyDir);
-		expect(empty).to.be(true);
+		expect(fs.isEmptySync(notEmptyDir)).to.be(false);
+		expect(fs.isEmptySync(emptyDir)).to.be(true);
 	});
 
-	it('should extends to fs', function (done) {
-		expect(fs.readdir).to.be.a('function');
-		done();
+	it('should extends to fs', function () {
+		expect(fs.readdir).to.be.a(Function);
 	});
 });
